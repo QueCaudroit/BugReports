588ea69c22a9e1f3b29e60a55e116a603f68d5a5
Quentin Caudroit <quentin.caudroit@free.fr>
2020-02-23 17:22:45 +0100

    easier install

---
+++ install.sh
@@ -0,0 +1,4 @@
+kubectl apply -f Kubernetes/DbDeployment.yaml
+kubectl apply -f Kubernetes/DbService.yaml
+kubectl apply -f Kubernetes/WebappDeployment.yaml
+kubectl apply -f Kubernetes/WebappService.yaml
